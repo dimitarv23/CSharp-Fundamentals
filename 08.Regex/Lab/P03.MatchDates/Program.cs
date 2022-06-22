adaa23615c3987e607a8252454833a84d41d3d8a
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-06-23 01:48:54 +0300

    Added Regex

---
+++ 08.Regex/Lab/P03.MatchDates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(\d{2})([\/|\.|-])([A-Z][a-z]{2})\2(\d{4})";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups[1]}, Month: {match.Groups[3]}, Year: {match.Groups[4]}");
+            }
+        }
+    }
+}
