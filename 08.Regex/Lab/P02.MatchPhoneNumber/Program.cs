3d05e903fe19267225987752c8a50411ce687808
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-06-23 01:46:41 +0300

    Added Regex

---
+++ 08.Regex/Lab/P02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\+359([ |-])2\1\d{3}\1\d{4}\b";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
