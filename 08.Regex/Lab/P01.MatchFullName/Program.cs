36b76a73e61a470a7604bd1ac106f27155f2af7c
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-06-23 01:45:14 +0300

    Added Regex

---
+++ 08.Regex/Lab/P01.MatchFullName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+[' '][A-Z][a-z]+";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
