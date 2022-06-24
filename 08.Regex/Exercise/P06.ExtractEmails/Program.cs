1fe4d07faa0713670e0dd88d616ac53a8918a07a
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-06-24 21:39:05 +0300

    Added Regex

---
+++ 08.Regex/Exercise/P06.ExtractEmails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P06.ExtractEmails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string emailPattern = @"(?<user>[a-z0-9\.\-_]+[a-z0-9]+)@(?<host>[a-z]+-*[a-z]+\.[a-z]+-*[a-z]+(\.[a-z]+-*[a-z]+)*)";
+
+            MatchCollection matches = Regex.Matches(text, emailPattern);
+
+            foreach (Match email in matches)
+            {
+                if (Char.IsLetterOrDigit(email.Value[0]))
+                {
+                    Console.WriteLine(email.Value);
+                }
+            }
+        }
+    }
+}
