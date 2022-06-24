2249fd7a05b3ece76c823e5a773af0d17684908b
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-06-24 21:37:36 +0300

    Added Regex

---
+++ 08.Regex/Exercise/P01.Furniture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace P01.Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<string> boughtFurniture = new List<string>();
+            decimal moneySpent = 0;
+            string pattern = @">>(?<type>[A-Za-z\s]+)<<(?<price>\d+(\.\d+)?)!(?<quantity>\d+)";
+
+            while (input != "Purchase")
+            {
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    string[] furnitureInfo = input
+                        .Split(new string[] { ">>", "<<", "!" }, StringSplitOptions.RemoveEmptyEntries);
+                    string furnitureType = match.Groups["type"].Value;
+                    decimal furniturePrice = decimal.Parse(match.Groups["price"].Value);
+                    int furnitureQuantity = int.Parse(match.Groups["quantity"].Value);
+
+                    boughtFurniture.Add(furnitureType);
+                    moneySpent += furniturePrice * furnitureQuantity;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bought furniture: ");
+
+            if (boughtFurniture.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, boughtFurniture));
+            }
+
+            Console.WriteLine($"Total money spend: {moneySpent:f2}");
+        }
+    }
+}
