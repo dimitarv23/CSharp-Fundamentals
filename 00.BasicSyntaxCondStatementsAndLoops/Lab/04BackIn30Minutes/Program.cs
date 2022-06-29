72fe5734e49d6e4b6fc2f7ceac6f8066939b43d8
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-06-29 18:31:52 +0300

    Added BasicSyntaxCondStatementsAndLoops

---
+++ 00.BasicSyntaxCondStatementsAndLoops/Lab/04BackIn30Minutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes -= 60;
+            }
+            if (hours >= 24)
+            {
+                hours = 0;
+            }
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
