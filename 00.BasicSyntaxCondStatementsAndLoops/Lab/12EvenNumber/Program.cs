038d859e0b91a1a8b7471faada6cc0f569509f1e
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-06-29 18:34:15 +0300

    Added BasicSyntaxCondStatementsAndLoops

---
+++ 00.BasicSyntaxCondStatementsAndLoops/Lab/12EvenNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _12EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            while (input % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                input = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {Math.Abs(input)}");
+        }
+    }
+}
