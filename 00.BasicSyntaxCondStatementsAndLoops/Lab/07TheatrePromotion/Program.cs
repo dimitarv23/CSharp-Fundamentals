e9c3a7c2dfdd8c45974ee3ca554384aed5a7601a
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-06-29 18:33:29 +0300

    Added BasicSyntaxCondStatementsAndLoops

---
+++ 00.BasicSyntaxCondStatementsAndLoops/Lab/07TheatrePromotion/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _07TheatrePromotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine();
+            int ageOfPerson = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            switch (typeOfDay)
+            {
+                case "Weekday":
+                    if (ageOfPerson >= 0 && ageOfPerson <= 18)
+                    {
+                        price = 12;
+                    }
+                    else if (ageOfPerson > 18 && ageOfPerson <= 64)
+                    {
+                        price = 18;
+                    }
+                    else if (ageOfPerson > 64 && ageOfPerson <= 122)
+                    {
+                        price = 12;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                        return;
+                    }
+                    break;
+
+                case "Weekend":
+                    if (ageOfPerson >= 0 && ageOfPerson <= 18)
+                    {
+                        price = 15;
+                    }
+                    else if (ageOfPerson > 18 && ageOfPerson <= 64)
+                    {
+                        price = 20;
+                    }
+                    else if (ageOfPerson > 64 && ageOfPerson <= 122)
+                    {
+                        price = 15;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                        return;
+                    }
+                    break;
+
+                case "Holiday":
+                    if (ageOfPerson >= 0 && ageOfPerson <= 18)
+                    {
+                        price = 5;
+                    }
+                    else if (ageOfPerson > 18 && ageOfPerson <= 64)
+                    {
+                        price = 12;
+                    }
+                    else if (ageOfPerson > 64 && ageOfPerson <= 122)
+                    {
+                        price = 10;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                        return;
+                    }
+                    break;
+            }
+            Console.WriteLine($"{price}$");
+        }
+    }
+}
