8be7af0617cafb90f16036e19328e932af1f9418
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-06-30 13:36:41 +0300

    Added BasicSyntaxCondStatementsAndLoops

---
+++ 00.BasicSyntaxCondStatementsAndLoops/Exercise/11. Orders/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double currentPrice = 0;
+            double totalPrice = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int numberOfCapsules = int.Parse(Console.ReadLine());
+
+                currentPrice = (days * numberOfCapsules) * pricePerCapsule;
+                totalPrice += currentPrice;
+
+                Console.WriteLine($"The price for the coffee is: ${currentPrice:f2}");
+            }
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
