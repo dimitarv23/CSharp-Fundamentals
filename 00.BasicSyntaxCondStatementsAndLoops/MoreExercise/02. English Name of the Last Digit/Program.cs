fc98a7adc76607daefeca4e393792ed09ad8478f
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-07-03 23:47:41 +0300

    Added BasicSyntaxCondStatementsAndLoops

---
+++ 00.BasicSyntaxCondStatementsAndLoops/MoreExercise/02. English Name of the Last Digit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02._English_Name_of_the_Last_Digit
+{
+    internal class Program
+    {
+        enum DigitName
+        {
+            zero,
+            one,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine
+        }
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            number %= 10;
+
+            Console.WriteLine((DigitName)number);
+        }
+    }
+}
