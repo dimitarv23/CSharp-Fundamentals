b4a9fc004266393b35bf62e876bc4255b4b4edf1
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-07-04 00:03:02 +0300

    Added MidExam3

---
+++ Mid Exams/Mid Exam 3/P01.CounterStrike/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P01.CounterStrike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+
+            int energyLeft = initialEnergy;
+            int battlesWon = 0;
+            string input = Console.ReadLine();
+
+            while (input != "End of battle")
+            {
+                int currEnergy = int.Parse(input);
+
+                if (energyLeft >= currEnergy)
+                {
+                    energyLeft -= currEnergy;
+                    battlesWon++;
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {battlesWon} won battles and {energyLeft} energy");
+                    break;
+                }
+
+                if (battlesWon % 3 == 0)
+                {
+                    energyLeft += battlesWon;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (input == "End of battle")
+            {
+                Console.WriteLine($"Won battles: {battlesWon}. Energy left: {energyLeft}");
+            }
+        }
+    }
+}
