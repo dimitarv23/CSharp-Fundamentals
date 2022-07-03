7822bbb6c9c0a0e0b6d2a80944a87666251a9c56
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-07-03 23:47:29 +0300

    Added BasicSyntaxCondStatementsAndLoops

---
+++ 00.BasicSyntaxCondStatementsAndLoops/MoreExercise/01. Sort Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._Sort_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int bigger = 0;
+
+            if (num1 < num2)
+            {
+                bigger = num2;
+                num2 = num1;
+                num1 = bigger;
+            }
+            if (num2 < num3)
+            {
+                bigger = num3;
+                num3 = num2;
+                num2 = bigger;
+            }
+            if (num1 < num2)
+            {
+                bigger = num2;
+                num2 = num1;
+                num1 = bigger;
+            }
+            Console.WriteLine($"{num1}\n{num2}\n{num3}");
+        }
+    }
+}
