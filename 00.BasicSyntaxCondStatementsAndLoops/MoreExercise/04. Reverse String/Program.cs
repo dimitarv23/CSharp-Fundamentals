60bf1981f5582bfd1b2f7326c6f9ae4cf0fd04eb
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-07-03 23:47:58 +0300

    Added BasicSyntaxCondStatementsAndLoops

---
+++ 00.BasicSyntaxCondStatementsAndLoops/MoreExercise/04. Reverse String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder reversed = new StringBuilder();
+            for (int i = 1; i <= input.Length; i++)
+            {
+                reversed.Append(input[input.Length - i]);
+            }
+            Console.WriteLine(reversed);
+        }
+    }
+}
