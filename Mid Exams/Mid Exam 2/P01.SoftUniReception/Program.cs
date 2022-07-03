6e4ef6ae5b5d9ce3e820d72fa203905493f9408d
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-07-04 00:02:07 +0300

    Added MidExam2

---
+++ Mid Exams/Mid Exam 2/P01.SoftUniReception/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P01.SoftUniReception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int secondEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int thirdEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            int efficiencyPerHour = firstEmployeeEfficiency + secondEmployeeEfficiency + thirdEmployeeEfficiency;
+            int timeNeeded = (int)Math.Ceiling(studentsCount / (double)efficiencyPerHour);
+
+            if (timeNeeded % 3 == 0 && timeNeeded != 0)
+            {
+                timeNeeded += timeNeeded / 3 - 1;
+            }
+            else
+            {
+                timeNeeded += timeNeeded / 3;
+            }
+
+            Console.WriteLine($"Time needed: {timeNeeded}h.");
+        }
+    }
+}
