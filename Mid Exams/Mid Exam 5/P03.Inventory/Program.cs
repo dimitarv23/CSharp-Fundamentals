d31470758efde9545c69323f867fbfd7e6b96f83
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-07-08 10:53:49 +0300

    Added MidExam5

---
+++ Mid Exams/Mid Exam 5/P03.Inventory/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inventory = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = Console.ReadLine();
+            while (command != "Craft!")
+            {
+                string[] cmdArgs = command
+                    .Split(new string[] { " - ", ":" }, StringSplitOptions.RemoveEmptyEntries);
+                string action = cmdArgs[0];
+                string item = cmdArgs[1];
+
+                if (action == "Collect")
+                {
+                    if (!inventory.Contains(item))
+                    {
+                        inventory.Add(item);
+                    }
+                }
+                else if (action == "Drop")
+                {
+                    if (inventory.Contains(item))
+                    {
+                        inventory.Remove(item);
+                    }
+                }
+                else if (action == "Combine Items")
+                {
+                    if (inventory.Contains(item))
+                    {
+                        string newItem = cmdArgs[2];
+                        int indexToCombine = inventory.IndexOf(item) + 1;
+                        inventory.Insert(indexToCombine, newItem);
+                    }
+                }
+                else if (action == "Renew")
+                {
+                    if (inventory.Contains(item))
+                    {
+                        inventory.Remove(item);
+                        inventory.Add(item);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", inventory));
+        }
+    }
+}
