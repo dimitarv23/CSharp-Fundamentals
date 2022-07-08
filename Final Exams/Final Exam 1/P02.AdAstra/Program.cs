48a6537dfd905e03f02b9d304902f1eff8ec23dc
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-07-09 02:53:54 +0300

    Added FinalExam1

---
+++ Final Exams/Final Exam 1/P02.AdAstra/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P02.AdAstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string pattern = @"([#|])(?<food>[A-Za-z ]+)\1(?<expirationDate>\d{2}\/\d{2}\/\d{2})\1(?<calories>\d+)\1";
+
+            MatchCollection matches = Regex.Matches(message, pattern);
+
+            int totalCalories = matches.Sum(match => int.Parse(match.Groups["calories"].Value));
+            Console.WriteLine($"You have food to last you for: {totalCalories / 2000} days!");
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Item: {match.Groups["food"]}, Best before: {match.Groups["expirationDate"]}, Nutrition: {match.Groups["calories"]}");
+            }
+        }
+    }
+}
