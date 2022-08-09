15f0b5f8a1c59482e7feabc7f3a6828ca3b6c76e
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-08-09 17:33:21 +0300

    Added FinalExam4

---
+++ Final Exams/Final Exam 4/P02.FancyBarcodes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace P02.FancyBarcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"(\@)[#]+[A-Z](?<productInfo>[A-Za-z0-9]{4,})[A-Z]\1[#]+";
+            
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    string productInfo = match.Groups["productInfo"].Value;
+                    string digits = GetDigits(productInfo);
+
+                    Console.WriteLine($"Product group: {(digits.Length == 0 ? "00" : digits)}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+
+        static string GetDigits(string text)
+        {
+            StringBuilder digits = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    digits.Append(text[i]);
+                }
+            }
+
+            return digits.ToString();
+        }
+    }
+}
